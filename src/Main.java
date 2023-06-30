ad9586cd3b7fdadecc9647d4e5c4dd40c19535a7
Anastasiya M <mega.gara11@gmail.com>
2023-06-30 13:55:18 +0700

    add src/

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 9800;
+        int bonusMile1 = 20;
+        int numberMile = (ticketPrice / bonusMile1);
+        System.out.println((numberMile) + " миль(ли) начислено");
+    }
+}
